cb3a623af96886ad929934198ca694c488a0c97b
Alex Kofke <adk62@case.edu>
2014-10-04 16:03:26 -0400

    changed things
    Signed-off-by: Alex Kofke <adk62@case.edu>

--- pr03/arrowlang_parser.py
+++ pr03/arrowlang_parser.py
@@ -13,15 +13,17 @@ class ArrowParser(object):
 
     def p_Start(self, p):
         'Start : Stmts'
-        p[0] = p[1]
+        p[0] = Node("Stmts").addkid(p[1])
 
     def p_Stmts(self, p):
         'Stmts : Stmts TStmt'
-        p[0] = p[1].addKid(p[2])
+        p[0] = p[1].addkid(p[2])
 
     def p_Stmts2(self, p):
         'Stmts : TStmt'
-        p[0] = Node("Stmt").addkid(p[1])
+        p[0] = p[1]
+
+    ###################################
 
     def p_TStmt(self, p):
         'TStmt : Stmt'
@@ -29,9 +31,37 @@ class ArrowParser(object):
 
     def p_TStmt2(self, p):
         'TStmt : FuncDefStmt'
-        p[0] = Node("FuncDef").addKid(p[1])
+        p[0] = Node("FuncDef").addkid(p[1])
+
+    ###################################
 
     def p_Stmt(self, p):
         'Stmt : CallStmt'
+        p[0] = Node("")
+
+    def p_Stmt2(self, p):
+        'Stmt : DeclStmt'
+        p[0] = p[1]
+
+    def p_Stmt3(self, p):
+        'Stmt : AssignStmt'
+        p[0] = p[1]
+
+    def p_Stmt4(self, p):
+        'Stmt : IfStmt'
+        p[0] = p[1]
+
+    def p_Stmt5(self, p):
+        'Stmt : WhileStmt'
+        p[0] = p[1]
+        
+    def p_Stmt6(self, p):
+        'Stmt : ForStmt'
+        p[0] = p[1]
+
+    ###################################
+
+    def p_Block(self, p):
+        'Block : LBRACE BlockStmts ReturnStmt RBRACE'
 
 
